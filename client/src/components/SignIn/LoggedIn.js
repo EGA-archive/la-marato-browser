234e465572ea184088bba65ba86fddf5c0105608
Giulia <giulia.cellerino@icloud.com>
2024-11-18 17:50:08 +0100

    Added Beacon Netowork logos, updated search button style and input, finished logout logic.

--- client/src/components/SignIn/LoggedIn.js
+++ client/src/components/SignIn/LoggedIn.js
@@ -1,30 +1,54 @@
 // client/src/components/SignIn/LoggedIn.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth } from 'oidc-react';
 import ExitToAppRoundedIcon from '@mui/icons-material/ExitToAppRounded';
+import DownloadDoneRoundedIcon from '@mui/icons-material/DownloadDoneRounded';
 
-const LoggedIn = ({ onClickHandler, imageSrc, setImageSrc }) => {
+const LoggedIn = ({ onClickHandler }) => {
   const [isActive, setIsActive] = useState(false);
+  const [logoutIsActive, setLogoutIsActive] = useState(false);
   const auth = useAuth();
 
   const handleLoginClick = () => {
     onClickHandler();
     setIsActive(true);
-    console.log("Button active state:", isActive)
+    console.log("Login handleLoginClick:", isActive) 
+    console.log("Logout handleLoginClick:", logoutIsActive)
   };
 
-  // const handleLoginRelease = () => {
-  //   setImageSrc('/../userimage.png');
+  // const handleLogoutClick = () => {
+  //   auth.signOut();
+  //   setLogoutIsActive(false); // Reset logout button state
+  //   console.log("Login handleLogoutClick:", isActive)
+  //   console.log("Logout handleLogoutClick:", logoutIsActive)
   // };
+
+const handleLogoutClick = () => {
+  setLogoutIsActive(true);
+}
+useEffect(() => {
+  if (logoutIsActive) {
+    setTimeout(() => auth.signOut(), 600); 
+    //      alert('You are logged out!')
+    // window.location.hash = ''
+  }
+}, [logoutIsActive]);
+
+
 
   if (auth && auth.userData) {
     return (
-  <button onClick={() => {
-    auth.signOut();
-    setIsActive(false); // Reset active state on logout
-  }} className="logout-button">
-    <ExitToAppRoundedIcon className="user-icon" alt="Quit Icon" /> Log Out
+  <button
+    className={`logout-button ${logoutIsActive ? 'logout-button-active' : ''}`} // Apply active class if isActive is true
+    onClick={handleLogoutClick}
+  >
+    <ExitToAppRoundedIcon
+          style={{ color: logoutIsActive ? '#902B43' : '' }} // Icon color based on active state
+    className="user-icon"
+        />
+        Log Out
   </button>
+
 );
 } else {
 // User is logged out, show the Log In button
